55ee4d8213270b7e167082959c5abcf049c0eeb0
raullupea <>
2024-01-29 11:02:50 +0000

    import random module to use random numbers for our dice and utilize Unicode Characters to build our dice structure

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+# We are going to utilize Unicode characters for building the dice structure
+
+#print("\u25CF \u250C \u2500 \u2510 \u2502 \u2514 \u2518 ")
+
+ #● ┌ ─ ┐ │ └ ┘ 
